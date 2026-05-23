e67cf67dc7e33641f25687d7256100582ead9b82
Philipp Waxweiler <philipp@waxweiler.de>
2026-05-23 19:09:31 +0200

    moved naka rushton calculation back to GPU because of precision problems in dark areas when using a LUT

--- TSTM Tonemapper/CoreImage-TSTM.swift
+++ TSTM Tonemapper/CoreImage-TSTM.swift
@@ -11,7 +11,7 @@ final class TSTMTonemapper: CIFilter {
 
     @objc dynamic var inputImage: CIImage?
     
-    private let context = CIContext()
+    private let context = CIContext(options: [.workingColorSpace: CGColorSpace(name: CGColorSpace.linearSRGB)!, .outputColorSpace: NSNull()])
 
     private static let kernel1: CIKernel = {
         let url = Bundle(for: TSTMTonemapper.self).url(forResource: "default", withExtension: "metallib")!
@@ -27,7 +27,7 @@ final class TSTMTonemapper: CIFilter {
         
         // 1. we need to fit gaussians into the image histogram of the luminance
         // According to S. Ferradans, fitting works better when using a log histogram
-        let lumVector = CIVector(x: 0.33, y: 0.33, z: 0.33, w: 0.0)
+        let lumVector = CIVector(x: 0.334, y: 0.334, z: 0.334, w: 0.0)
         
         let multiplyFilter = CIFilter.colorMatrix()
         multiplyFilter.inputImage = input
@@ -44,18 +44,7 @@ final class TSTMTonemapper: CIFilter {
         let (segmentationBorders, means) = segmentImage(lumImage: multiplyFilter.outputImage!, luminanceParams: globLuminance)
         
         // 3. calculate all input parameters for the tone mapping algorithm
-        let f_G = calcToneCurve(globLuminance: globLuminance,
-                                segBorders: segmentationBorders,
-                                means: means)
-        
-        let lut = f_G.withUnsafeBufferPointer { Data(buffer: $0) }
-        let lutImage = CIImage(bitmapData: lut,
-                               bytesPerRow: MemoryLayout<Float32>.size * f_G.count,
-                               size: CGSize(width: f_G.count, height: 1), // 256x1 pixel
-                               format: .Rf,
-                               colorSpace: nil)
-        
-        let texture = lutImage.metalTexture // this is nil
+        let tstmParams = TstmParameters(globLuminance: globLuminance, segBorders: segmentationBorders, means: means)
         
         // 3. Apply Naka-Rushton equation
         let result = Self.kernel1.apply(
@@ -63,9 +52,14 @@ final class TSTMTonemapper: CIFilter {
             roiCallback: { _, rect in rect },
             arguments: [input,
                         multiplyFilter.outputImage!,
-                        lutImage,
-                        globLuminance.min,
-                        globLuminance.max
+                        segmentationBorders.map({$0.lower}).withUnsafeBufferPointer { Data(buffer: $0) },
+                        segmentationBorders.map({$0.upper}).withUnsafeBufferPointer { Data(buffer: $0) },
+                        tstmParams.m_j.withUnsafeBufferPointer { Data(buffer: $0) },
+                        tstmParams.k_j.withUnsafeBufferPointer { Data(buffer: $0) },
+                        tstmParams.C_j.withUnsafeBufferPointer { Data(buffer: $0) },
+                        tstmParams.c.withUnsafeBufferPointer { Data(buffer: $0) },
+                        tstmParams.h.withUnsafeBufferPointer { Data(buffer: $0) },
+                        UInt32(means.count)
                        ]
         )!
         
@@ -99,7 +93,7 @@ final class TSTMTonemapper: CIFilter {
                        rowBytes: MemoryLayout<SIMD4<Float32>>.size * loghistogramValues.count,
                        bounds: logHistogramFilter.outputImage!.extent,
                        format: .RGBAf,
-                       colorSpace: CGColorSpace(name: CGColorSpace.linearSRGB)!)
+                       colorSpace: nil)
         
         retLogHistogram = loghistogramValues.map({$0.x}) // only extract one component (they are all equal anyway)
         
@@ -149,10 +143,23 @@ final class TSTMTonemapper: CIFilter {
         return (segmentationBorders, gmm_means_lin)
     }
     
-    private func calcToneCurve(globLuminance: GlobalLuminanceParameters, segBorders: [SegmentationBorder], means: [Float]) -> [Float32]
+    
+} // end of class
+
+// parameter structs
+
+private struct TstmParameters
+{
+    let h: [Float]
+    let c: [Float]
+    let m_j: [Float]
+    let k_j: [Float]
+    let C_j: [Float]
+    
+    init(globLuminance: GlobalLuminanceParameters, segBorders: [SegmentationBorder], means: [Float])
     {
         // calculate m from the luminance average, this is needed in eq. (23)
-        let m = (pow(globLuminance.µ, 2.0) - (globLuminance.max * globLuminance.min)) / (globLuminance.max + globLuminance.min - (2 * globLuminance.µ)) // eq. (20)
+        let m:Float = (pow(globLuminance.µ, 2.0) - (globLuminance.max * globLuminance.min)) / (globLuminance.max + globLuminance.min - (2 * globLuminance.µ)) // eq. (20)
         
         // h_j eq. (23)
         let h_array:[Float] = segBorders.map
@@ -160,66 +167,33 @@ final class TSTMTonemapper: CIFilter {
             log( (m + border.upper) / (m + border.lower) )
         }
         let h_sum = h_array.reduce(0, +)
-        let h = h_array.map{$0 / h_sum}
+        let h_init = h_array.map{$0 / h_sum}
         
         // c_j
-        let c = [0.0] + (1..<h.endIndex).map{ j in h[0...(j - 1)].reduce(0, +) }
+        let c_init = [0.0] + (1..<h_init.endIndex).map{ j in h_init[0...(j - 1)].reduce(0, +) }
         
         // parameters per segment j
-        let m_ = zip(segBorders, means).map({ segment, mean in
+        let m_j_init = zip(segBorders, means).map({ segment, mean in
             let m_j = ((pow(mean, 2.0) - (segment.upper * segment.lower)) ) / (segment.upper + segment.lower - (2.0 * mean)); // equation (20)
-            return max(1e-8,  m_j); // equation (20) part 2. Cannot be 0 because equation (12) contains a log(x) where x cannot be 0
+            return max(1e-12,  m_j); // equation (20) part 2. Cannot be 0 because equation (12) contains a log(x) where x cannot be 0
         })
         
-        let k_ = zip(segBorders, m_).map({ segment, m_j in
+        let k_j_init = zip(segBorders, m_j_init).map({ segment, m_j in
             1.0 / log( (m_j + segment.upper) / (m_j + segment.lower) );   // equation (19)
         })
 
-        let C_ = zip(zip(k_, m_), means).map({ params, µ in
+        let C_j_init = zip(zip(k_j_init, m_j_init), means).map({ params, µ in
             let (k_j, m_j) = params
             return 0.5 - (k_j * log(m_j * µ));  // equation (13)
         })
-
-        // the lightness perception function f_G (eq. (24) in the paper) will be precalculated for the GPU
-        let lumVals = stride(from: globLuminance.min, through: globLuminance.max, by: (globLuminance.max - globLuminance.min) / 255.0)
         
-        let f_G:[Float32] = lumVals.map({ luminance in
-            var r_G: Float32 = 0.0
-            for (j, segment) in segBorders.enumerated()
-            {
-                if( segment.contains(x: luminance) )
-                {
-                    let nakaRushton_r = normNakaRushtonEquation(lambda: luminance,
-                                                                m_j: m_[j],
-                                                                k_j: k_[j],
-                                                                C: C_[j],
-                                                                segment: segment)
-                    
-                    r_G += c[j] + (h[j] * nakaRushton_r); // equation (21)
-                } // else: nothing
-            }
-            return (luminance / (r_G + 1e-8)) - luminance; // equation (24)
-        })
-        
-        return f_G;
+        self.h = h_init
+        self.c = c_init
+        self.m_j = m_j_init
+        self.k_j = k_j_init
+        self.C_j = C_j_init
     }
-    
-    private func normNakaRushtonEquation(lambda: Float,
-                                         m_j: Float,
-                                         k_j: Float,
-                                         C: Float,
-                                         segment: SegmentationBorder) -> Float
-    {
-        
-        let r_luminance = C + (k_j * log(m_j + lambda)); // equation (12)
-        let r_luminance_min = C + (k_j * log(m_j + segment.lower));
-        let r_luminance_max = C + (k_j * log(m_j + segment.upper));
-        
-        return (r_luminance - r_luminance_min) / (r_luminance_max - r_luminance_min); // equation (18)
-    }
-} // end of class
-
-// parameter structs
+}
 
 extension GlobalLuminanceParameters
 {    
@@ -239,7 +213,7 @@ extension GlobalLuminanceParameters
                        rowBytes: MemoryLayout<SIMD4<Float32>>.size * minMax.count,
                        bounds: minMaxFilter.outputImage!.extent,
                        format: .RGBAf,
-                       colorSpace: CGColorSpace(name: CGColorSpace.linearSRGB)!)
+                       colorSpace: nil)
         
         var avg:SIMD4<Float32> = .zero
         context.render(avgFilter.outputImage!,
@@ -247,7 +221,7 @@ extension GlobalLuminanceParameters
                        rowBytes: MemoryLayout<SIMD4<Float32>>.size,
                        bounds: avgFilter.outputImage!.extent,
                        format: .RGBAf,
-                       colorSpace: CGColorSpace(name: CGColorSpace.linearSRGB)!)
+                       colorSpace: nil)
         
         self.min = minMax.first!.x
         self.max = minMax.last!.x
--- TSTM Tonemapper/TSTM.ci.metal
+++ TSTM Tonemapper/TSTM.ci.metal
@@ -9,27 +9,59 @@
 using namespace metal;
 #include <CoreImage/CoreImage.h>
 
+float normNakaRushtonEquation(const float lambda,
+                              const float m_j,
+                              const float k_j,
+                              const float C,
+                              const float minLum,
+                              const float maxLum)
+{
+    const float r_luminance = C + (k_j * log(m_j + lambda)); // equation (12)
+    const float r_luminance_min = C + (k_j * log(m_j + minLum));
+    const float r_luminance_max = C + (k_j * log(m_j + maxLum));
+    
+    return (r_luminance - r_luminance_min) / (r_luminance_max - r_luminance_min); // equation (18)
+}
+
+bool contains(const float value, const float lowerBorder, const float upperBorder)
+{
+    return ( (lowerBorder <= value) && (value <= upperBorder) );
+}
+
 extern "C"
 {
     namespace coreimage
     {
         float4 tonemap(coreimage::sample_t color,       // primary RGB image
                        coreimage::sample_t luminance,   // luminance image
-                       coreimage::sampler lightnessLUT, // a 256 x 1 image
-                       const float minLuminance,
-                       const float maxLuminance)
+                       constant float * lowerSegmentBorders,
+                       constant float * upperSegmentBorders,
+                       constant float * m_,
+                       constant float * k_,
+                       constant float * C_,
+                       constant float * c,
+                       constant float * h,
+                       const unsigned numSegments)
         {
-            // sampler extent (x, y, width, height)
-            float4 extent = samplerExtent(lightnessLUT);
-
-            // luminance is in range [0,1] like the sampler coordinate system
-            const float lutPos = ((luminance.r - minLuminance) / (maxLuminance - minLuminance)) * extent.z;
-                        
-            // y=0.5 so we are dead center on the LUT's y axis
-            float f_G = lightnessLUT.sample(lightnessLUT.transform(float2(lutPos, 0.5f * extent.w) + extent.xy)).r;
+            float r_G = 0.f;
             
-            float3 rgb = color.rgb;
-            float3 toneMapped = rgb / (rgb + f_G + 1e-8);
+            for (unsigned j = 0; j < numSegments; j++)
+            {
+                if( contains(luminance.r, lowerSegmentBorders[j], upperSegmentBorders[j]) )
+                {
+                    const float nakaRushton_r = normNakaRushtonEquation(luminance.r,
+                                                                        m_[j],
+                                                                        k_[j],
+                                                                        C_[j],
+                                                                        lowerSegmentBorders[j],
+                                                                        upperSegmentBorders[j]);
+                    
+                    r_G += c[j] + (h[j] * nakaRushton_r); // equation (21)
+                } // else: nothing
+            }
+            const float f_G = (luminance.r / (r_G + 1e-12)) - luminance.r; // equation (24)
+            
+            const float3 toneMapped = color.rgb / (color.rgb + f_G + 1e-12);
             
             return float4(toneMapped, color.a);
         }
